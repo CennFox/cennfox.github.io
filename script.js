0e652d6145d29771e98c8d58dd1f41d256090801
CennFox <136531054+CennFox@users.noreply.github.com>
2023-06-13 19:08:13 -0500

    functionality update

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-var scene = document.getElementById('scene');
 var postcard = document.getElementById('postcard');
-var isDragging = false;
-var startX = 0, startY = 0;
-var currentX = 0, currentY = 0;
+var flipButton = document.getElementById('flip-button');
+var isFront = true;
 
-scene.addEventListener('mousedown', function(event) {
-    isDragging = true;
-    startX = event.clientX - currentX;
-    startY = event.clientY - currentY;
-});
+function flipPostcard() {
+    if (isFront) {
+        postcard.style.transform = 'rotateY(180deg)';
+    } else {
+        postcard.style.transform = 'rotateY(0deg)';
+    }
+    isFront = !isFront;
+}
 
-scene.addEventListener('mousemove', function(event) {
-    if (!isDragging) return;
-    currentX = event.clientX - startX;
-    currentY = event.clientY - startY;
-    postcard.style.transform = 'rotateX(' + currentY + 'deg) rotateY(' + currentX + 'deg)';
-});
-
-scene.addEventListener('mouseup', function() {
-    isDragging = false;
-});
+flipButton.addEventListener('click', flipPostcard);
