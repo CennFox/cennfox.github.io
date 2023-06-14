13fb690fc3fbd6065298b7b65ce018f0a66e5731
CennFox <136531054+CennFox@users.noreply.github.com>
2023-06-13 20:47:10 -0500

    added the actual content

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var postcard = document.getElementById('postcard');
 var flipButton = document.getElementById('flip-button');
+var swapButton = document.getElementById('swap-button');
+var selectCard = document.getElementById('select-card');
+var frontImage = document.getElementById('front-image');
+var backImage = document.getElementById('back-image');
 var isFront = true;
 
 function flipPostcard() {
@@ -11,4 +15,30 @@ function flipPostcard() {
     isFront = !isFront;
 }
 
+function updateCard() {
+    var selected = selectCard.value;
+    frontImage.src = 'front' + selected + '.png';
+    backImage.src = 'back' + selected + '.png';
+    checkSwapImage();
+}
+
+function swapImage() {
+    var selected = selectCard.value;
+    frontImage.src = 'front' + selected + 'b.png';
+}
+
+function checkSwapImage() {
+    var selected = selectCard.value;
+    var img = new Image();
+    img.src = 'front' + selected + 'b.png';
+    img.onload = function() {
+        swapButton.disabled = false;
+    }
+    img.onerror = function() {
+        swapButton.disabled = true;
+    }
+}
+
 flipButton.addEventListener('click', flipPostcard);
+swapButton.addEventListener('click', swapImage);
+selectCard.addEventListener('change', updateCard);
