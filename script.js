8bc01a4292e6e672986ce493e1195ba145c58078
CennFox <136531054+CennFox@users.noreply.github.com>
2023-06-17 10:16:45 -0500

    BETA Two cards mode advanced placement

--- script.js
+++ script.js
@@ -1,44 +1,94 @@
-var postcard = document.getElementById('postcard');
-var flipButton = document.getElementById('flip-button');
-var swapButton = document.getElementById('swap-button');
-var selectCard = document.getElementById('select-card');
-var frontImage = document.getElementById('front-image');
-var backImage = document.getElementById('back-image');
-var isFront = true;
+window.addEventListener('DOMContentLoaded', () => {
+    const postcards = Array.from(document.getElementsByClassName('postcard'));
+    const flipButton = document.getElementById('flip-button');
+    const advancedButton = document.getElementById('advanced-button');
+    const blackoutButton = document.getElementById('blackout-button');
+    const selectCards = Array.from(document.getElementsByClassName('select-card'));
+    const frontImages = Array.from(document.getElementsByClassName('front-image'));
+    const backImages = Array.from(document.getElementsByClassName('back-image'));
 
-function flipPostcard() {
-    if (isFront) {
-        postcard.style.transform = 'rotateY(180deg)';
-    } else {
-        postcard.style.transform = 'rotateY(0deg)';
+    let isFront = [true, true];
+    let isAdvanced = false;
+
+    function flipPostcards() {
+        postcards.forEach((postcard, index) => {
+            if (isFront[index]) {
+                postcard.style.transform = "rotateY(180deg)";
+                isFront[index] = false;
+            } else {
+                postcard.style.transform = "rotateY(0deg)";
+                isFront[index] = true;
+            }
+        });
     }
-    isFront = !isFront;
+
+function changeImage(e) {
+    const cardNumber = e.target.value;
+    const index = selectCards.indexOf(e.target);
+
+    fetch(`front${cardNumber}.png`).then(response => {
+        if (response.ok) {
+            frontImages[index].src = `front${cardNumber}.png`;
+        } else {
+            frontImages[index].src = `front69420.png`;
+        }
+    }).catch(() => {
+        frontImages[index].src = `front69420.png`;
+    });
+
+    fetch(`back${cardNumber}.png`).then(response => {
+        if (response.ok) {
+            backImages[index].src = `back${cardNumber}.png`;
+        } else {
+            backImages[index].src = `back69420.png`;
+        }
+    }).catch(() => {
+        backImages[index].src = `back69420.png`;
+    });
+
+    checkBlackout(index);
 }
 
-function updateCard() {
-    var selected = selectCard.value;
-    frontImage.src = 'front' + selected + '.png';
-    backImage.src = 'back' + selected + '.png';
-    checkSwapImage();
-}
 
-function swapImage() {
-    var selected = selectCard.value;
-    frontImage.src = 'front' + selected + 'b.png';
-}
+    function toggleAdvancedMode() {
+        const postcard2 = document.getElementById('postcard2');
+        const selectCard2 = document.getElementById('select-card2');
 
-function checkSwapImage() {
-    var selected = selectCard.value;
-    var img = new Image();
-    img.src = 'front' + selected + 'b.png';
-    img.onload = function() {
-        swapButton.disabled = false;
+        if (isAdvanced) {
+            postcard2.style.display = 'none';
+            selectCard2.style.display = 'none';
+            isAdvanced = false;
+        } else {
+            postcard2.style.display = 'flex';
+            selectCard2.style.display = 'inline-block';
+            isAdvanced = true;
+        }
     }
-    img.onerror = function() {
-        swapButton.disabled = true;
-    }
-}
 
-flipButton.addEventListener('click', flipPostcard);
-swapButton.addEventListener('click', swapImage);
-selectCard.addEventListener('change', updateCard);
+    function checkBlackout() {
+        const currentCard = document.getElementById('select-card1').value;
+        fetch(`front${currentCard}b.png`).then(response => {
+            if (response.ok) {
+                blackoutButton.disabled = false;
+            } else {
+                blackoutButton.disabled = true;
+            }
+        });
+    }
+
+    function blackout() {
+        const currentCard = document.getElementById('select-card1').value;
+        frontImages[0].src = `front${currentCard}b.png`;
+        checkBlackout();
+    }
+
+    flipButton.addEventListener('click', flipPostcards);
+    advancedButton.addEventListener('click', toggleAdvancedMode);
+    blackoutButton.addEventListener('click', blackout);
+    selectCards.forEach(selectCard => selectCard.addEventListener('change', changeImage));
+    
+    // Initially hide the second postcard
+    document.getElementById('postcard2').style.display = 'none';
+    // Check blackout on page load
+    checkBlackout();
+});
